16cba09a592c98121168efa0b631a78667bdb445
Wulandr <wdari0161@gmail.com>
2021-11-21 21:38:51 +0700

    menambahkan background di forum

--- lib/view/forum/forum.dart
+++ lib/view/forum/forum.dart
@@ -40,195 +40,208 @@ class _ForumState extends State<Forum> {
     getProducts();
     return Container(
       decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            "assets/image/bg3.jpg",
+          ),
+          fit: BoxFit.cover,
+        ),
         gradient:
             RadialGradient(colors: [Colors.blue.shade100, Colors.white10]),
       ),
-      margin: EdgeInsets.only(left: 28.8, right: 28.8, top: 28.8),
-      child: FutureBuilder(
-        future: getProducts(),
-        builder: (context, snapshot) {
-          return ListView.builder(
-              itemCount: _list.length,
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    if ((_list[index]["parent"]) == 0)
-                      Column(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.purple.withOpacity(.2),
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Column(
-                                children: [
-                                  Icon(
-                                    Icons.app_registration_rounded,
-                                    size: 50,
-                                    color: Colors.purple,
-                                  ),
-                                  Text(
-                                    _list[index]["kategori"],
-                                    style: TextStyle(
-                                        fontSize: 13, color: Colors.black),
-                                  ),
-                                  if (_list[index]["kategori"] ==
-                                      "Manajemen SPBE")
-                                    Container(
-                                      width: 130,
-                                      child: Column(
-                                        children: [
-                                          Text("- Manajemen Risiko SPBE",
-                                              style: TextStyle(
-                                                  fontSize: 11,
-                                                  color: Colors.purple)),
-                                          Text("- Manajemen SDM SPBE",
-                                              style: TextStyle(
-                                                  fontSize: 11,
-                                                  color: Colors.purple))
-                                        ],
-                                      ),
-                                    ),
-                                  if (_list[index]["kategori"] ==
-                                      "Layanan Publik")
-                                    Container(
-                                      width: 130,
-                                      child: Column(
-                                        children: [
-                                          Text("- Aduan Masyarakat",
-                                              style: TextStyle(
-                                                  fontSize: 11,
-                                                  color: Colors.purple)),
-                                          Text("- Satu Data",
-                                              style: TextStyle(
-                                                  fontSize: 11,
-                                                  color: Colors.purple)),
-                                          Text("- Produk Hukum",
-                                              style: TextStyle(
-                                                  fontSize: 11,
-                                                  color: Colors.purple)),
-                                        ],
-                                      ),
-                                    ),
-                                ],
-                              ),
-                            ),
-                          ),
-
-                          SizedBox(
-                            height: 20,
-                          ),
-
-                          // Row(
-                          //   children: [
-                          //     TextButton(
-                          //       child: Text(
-                          //         _list[index]["id"] +
-                          //             ". " +
-                          //             _list[index]["kategori"],
-                          //       ),
-                          //       onPressed: () {
-                          //         Navigator.pushNamed(context, '/list');
-                          //       },
-                          //     ),
-                          //   ],
-                          // ),
-                        ],
-                      ),
-                    if ((_list[index]["kategori"]) == "Manajemen SPBE")
-                      Container(
-                        // padding: EdgeInsets.all(15),
-                        decoration: BoxDecoration(
-                          // color: Colors.grey[300],
-                          borderRadius: new BorderRadius.circular(10),
-                        ),
-                        child: Column(children: [
-                          // Text("a. Manajemen Risiko SPBE"),
-                          // Text("b. Manajemen SDM SPBE"),
-                        ]),
-                      ),
-                    if ((_list[index]["kategori"]) == "Layanan Publik")
-                      Container(
-                        padding: EdgeInsets.all(15),
-                        decoration: BoxDecoration(
-                          // color: Colors.grey[300],
-                          borderRadius: new BorderRadius.circular(10),
-                        ),
-                        child: Column(children: [
-                          // Text("a. Manajemen Risiko SPBE"),
-                          // Text("b. Manajemen SDM SPBE"),
-                        ]),
-                      ),
-                    if ((_list[index]["level"]) == 2 &&
-                        (_list[index]["parent"]) >= 1)
-                      // if ((_list[index]["parent"]).toString() == 2)
-                      Container(
-                        margin: EdgeInsets.only(top: 3),
-                        child: Column(
+      child: Container(
+        margin: EdgeInsets.only(left: 28.8, right: 28.8, top: 28.8),
+        child: FutureBuilder(
+          future: getProducts(),
+          builder: (context, snapshot) {
+            return ListView.builder(
+                itemCount: _list.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      if ((_list[index]["parent"]) == 0)
+                        Column(
                           children: [
-                            Card(
-                              child: Column(
-                                children: [
-                                  Container(
-                                    margin: EdgeInsets.only(top: 3),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(.2),
-                                      child: Column(
-                                        children: [
-                                          // Icon(
-                                          //   Icons.book_outlined,
-                                          //   color: Colors.blue,
-                                          //   size: .1,
-                                          // ),
-                                          TextButton(
-                                            autofocus: false,
-                                            onPressed: () {
-                                              Navigator.pushNamed(
-                                                  context, '/list', arguments: {
-                                                'kategori': _list[index]
-                                                    ["kategori"]
-                                              });
-                                            },
-                                            child: Center(
-                                              child: Text(
-                                                _list[index]["kategori"],
+                            Container(
+                              width: 300,
+                              height: 120,
+                              decoration: BoxDecoration(
+                                color: Colors.blue.withOpacity(.2),
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Column(
+                                  children: [
+                                    Icon(
+                                      Icons.chat_rounded,
+                                      size: 30,
+                                      color: Colors.blue,
+                                    ),
+                                    Text(
+                                      " " + _list[index]["kategori"],
+                                      style: TextStyle(
+                                          fontSize: 13, color: Colors.black),
+                                    ),
+                                    if (_list[index]["kategori"] ==
+                                        "Manajemen SPBE")
+                                      Container(
+                                        margin: EdgeInsets.all(5),
+                                        width: 140,
+                                        child: Column(
+                                          children: [
+                                            Text("1. Manajemen Risiko SPBE",
                                                 style: TextStyle(
-                                                    color: Colors.blue,
-                                                    fontSize: 12),
-                                              ),
-                                            ),
-                                          ),
-                                        ],
+                                                    fontSize: 11,
+                                                    color: Colors.blue)),
+                                            Text("2. Manajemen SDM SPBE",
+                                                style: TextStyle(
+                                                    fontSize: 11,
+                                                    color: Colors.blue))
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                    width: 250,
-                                    height: 49,
-                                    decoration: new BoxDecoration(
-                                      color: Colors.blue.withOpacity(.2),
-                                      // // color: new Color(0xFF333366),
-                                      // shape: BoxShape.rectangle,
-                                      borderRadius:
-                                          new BorderRadius.circular(10),
-                                      // boxShadow: <BoxShadow>[
-                                      //   new BoxShadow(
-                                      //       color: Colors.black12,
-                                      //       blurRadius: 10,
-                                      //       offset: new Offset(0.0, 2.0))
-                                      // ]
-                                    ),
-                                  ),
-                                ],
+                                    if (_list[index]["kategori"] ==
+                                        "Layanan Publik")
+                                      Container(
+                                        margin: EdgeInsets.all(5),
+                                        width: 130,
+                                        child: Column(
+                                          children: [
+                                            Text("1.Aduan Masyarakat",
+                                                style: TextStyle(
+                                                    fontSize: 11,
+                                                    color: Colors.blue)),
+                                            Text("2.Satu Data",
+                                                style: TextStyle(
+                                                    fontSize: 11,
+                                                    color: Colors.blue)),
+                                            Text("3.Produk Hukum",
+                                                style: TextStyle(
+                                                    fontSize: 11,
+                                                    color: Colors.blue)),
+                                          ],
+                                        ),
+                                      ),
+                                  ],
+                                ),
                               ),
                             ),
+
+                            SizedBox(
+                              height: 20,
+                            ),
+
+                            // Row(
+                            //   children: [
+                            //     TextButton(
+                            //       child: Text(
+                            //         _list[index]["id"] +
+                            //             ". " +
+                            //             _list[index]["kategori"],
+                            //       ),
+                            //       onPressed: () {
+                            //         Navigator.pushNamed(context, '/list');
+                            //       },
+                            //     ),
+                            //   ],
+                            // ),
                           ],
                         ),
-                      ),
-                  ],
-                );
-              });
-        },
+                      if ((_list[index]["kategori"]) == "Manajemen SPBE")
+                        Container(
+                          // padding: EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                            // color: Colors.grey[300],
+                            borderRadius: new BorderRadius.circular(10),
+                          ),
+                          child: Column(children: [
+                            // Text("a. Manajemen Risiko SPBE"),
+                            // Text("b. Manajemen SDM SPBE"),
+                          ]),
+                        ),
+                      if ((_list[index]["kategori"]) == "Layanan Publik")
+                        Container(
+                          padding: EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                            // color: Colors.grey[300],
+                            borderRadius: new BorderRadius.circular(10),
+                          ),
+                          child: Column(children: [
+                            // Text("a. Manajemen Risiko SPBE"),
+                            // Text("b. Manajemen SDM SPBE"),
+                          ]),
+                        ),
+                      if ((_list[index]["level"]) == 2 &&
+                          (_list[index]["parent"]) >= 1)
+                        // if ((_list[index]["parent"]).toString() == 2)
+                        Container(
+                          margin: EdgeInsets.only(top: 3),
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(top: 3),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(.2),
+                                        child: Column(
+                                          children: [
+                                            // Icon(
+                                            //   Icons.book_outlined,
+                                            //   color: Colors.blue,
+                                            //   size: .1,
+                                            // ),
+                                            TextButton(
+                                              autofocus: false,
+                                              onPressed: () {
+                                                Navigator.pushNamed(
+                                                    context, '/list',
+                                                    arguments: {
+                                                      'kategori': _list[index]
+                                                          ["kategori"]
+                                                    });
+                                              },
+                                              child: Center(
+                                                child: Text(
+                                                  _list[index]["kategori"],
+                                                  style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontSize: 12),
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      width: 200,
+                                      height: 49,
+                                      decoration: new BoxDecoration(
+                                        color: Colors.blue.withOpacity(.8),
+                                        // // color: new Color(0xFF333366),
+                                        // shape: BoxShape.rectangle,
+                                        borderRadius:
+                                            new BorderRadius.circular(10),
+                                        // boxShadow: <BoxShadow>[
+                                        //   new BoxShadow(
+                                        //       color: Colors.black12,
+                                        //       blurRadius: 10,
+                                        //       offset: new Offset(0.0, 2.0))
+                                        // ]
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                    ],
+                  );
+                });
+          },
+        ),
       ),
     );
   }
